af2a0b38fef7a525ba44dc9a8fcdcfe153fdc24f
ROHINIVYAS <vyasrohini2@gmail.com>
2024-06-13 11:48:01 +0530

    Signup code added

--- src/main/java/com/example/userservicemar24/controllers/AuthController.java
+++ src/main/java/com/example/userservicemar24/controllers/AuthController.java
@@ -1,8 +1,10 @@
 package com.example.userservicemar24.controllers;
 
 import com.example.userservicemar24.dtos.LoginRequestDto;
+import com.example.userservicemar24.dtos.SignUpRequestDto;
 import com.example.userservicemar24.dtos.UserDto;
 import com.example.userservicemar24.services.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,8 +17,14 @@ public class AuthController {
     }
     @PostMapping("/login")
     public ResponseEntity<UserDto> login(@RequestBody LoginRequestDto requestDto) {
+        UserDto userDto=authService.login(requestDto.getEmail(),requestDto.getPassword());
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
+    }
 
-        return null;
+    @PostMapping("/signup")
+    public ResponseEntity<UserDto> signup(@RequestBody SignUpRequestDto requestDto) {
+        UserDto userDto = authService.signup(requestDto.getEmail(),requestDto.getPassword());
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
     }
 
 
--- src/main/java/com/example/userservicemar24/services/AuthService.java
+++ src/main/java/com/example/userservicemar24/services/AuthService.java
@@ -1,7 +1,37 @@
 package com.example.userservicemar24.services;
 
+import com.example.userservicemar24.dtos.UserDto;
+import com.example.userservicemar24.models.User;
+import com.example.userservicemar24.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthService {
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    public AuthService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+    public UserDto signup(String email, String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(bCryptPasswordEncoder.encode(password));
+        User savedUser = userRepository.save(user);
+        return UserDto.from(savedUser);
+    }
+
+    public UserDto login(String email, String password) {
+        Optional<User> user = userRepository.findByEmail(email);
+        if(user.isEmpty()){
+            return null;
+        }
+        if(!bCryptPasswordEncoder.matches(password, user.get().getPassword())){
+            throw  new RuntimeException("Password is incorrect");
+        }
+        return UserDto.from(user.get());
+    }
 }
